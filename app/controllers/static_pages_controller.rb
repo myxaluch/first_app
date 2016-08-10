7ab3e87129634185ec497575990fc903aab727ea
Alexander Kotov <amikotov@gmail.com>
2016-08-10 15:55:09 +0300

    Add alert for not-found exception and empty search input

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,7 +3,14 @@ class StaticPagesController < ApplicationController
   end
 
   def search
-    @query = params[:value].upcase
-    @result = FoodAdditive.search_E @query
+    if params[:value].empty?
+      flash[:danger] = "Пожалуйста, введите название добавки"
+      redirect_to root_url
+    else
+      @query = params[:value].upcase
+      @result = FoodAdditive.search_E @query
+    end
   end
+
+
 end
