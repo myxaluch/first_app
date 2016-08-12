23335bd0aaa9e5e5219532b6d9be59c52ef545d0
Alexander Kotov <amikotov@gmail.com>
2016-08-12 12:14:43 +0300

    [WIP] Add logic for update and destroy elements from DB

--- app/controllers/admin/food_additives_controller.rb
+++ app/controllers/admin/food_additives_controller.rb
@@ -4,7 +4,7 @@ class Admin::FoodAdditivesController < ApplicationController
 
   def index
     @rand = FoodAdditive.take
-    @food_additives = FoodAdditive.paginate(page: params[:page])
+    @food_additives = FoodAdditive.paginate(page: params[:page]).order(:name)
   end
 
   def new
@@ -23,9 +23,26 @@ class Admin::FoodAdditivesController < ApplicationController
   end
 
   def update
+    @additive = FoodAdditive.find(params[:id])
+    if @additive.update_attributes(additive_params)
+      flash[:success] = "Информация обновлена"
+      redirect_to admin_food_additive_path(@additive.id)
+    else
+      render 'show'
+    end
   end
 
   def destroy
+    FoodAdditive.find(params[:id]).destroy
+    flash[:success] = "Информация удалена"
+    redirect_to admin_path
   end
+
+
+    private
+
+    def additive_params
+      params.require(:food_additive).permit(:name, :about)
+    end
 
 end
